536f79f7153b9c07108bff04c5bf5047a75b9292
munarabaisalova <munarabaisalova12@gmail.com>
2023-12-04 19:56:19 +0600

    first commit

---
+++ src/components/CardTodos/index.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+
+
+const CardTodos = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const addTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, { id: Date.now(), text: newTask, completed: false }]);
+      setNewTask('');
+    }
+  };
+
+  const toggleTask = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, completed: !task.completed } : task
+      )
+    );
+  };
+
+  const removeTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  return (
+    <div className="container">
+      <h1>Todo List App</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="new ..."
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+        />
+        <button onClick={addTask}>Add</button>
+      </div>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            <input
+              type="checkbox"
+             
+              checked={task.completed}
+              onChange={() => toggleTask(task.id)}
+            />
+            <span
+              style={{ textDecoration: task.completed ? 'line-through' : 'none' }}
+            >
+              {task.text}
+            </span>
+            <button onClick={() => removeTask(task.id)}>delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default CardTodos;
